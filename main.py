022cc22f7172b2a68106c0264ad6691614f94622
Sandro Silva <sandro2007am@gmail.com>
2024-04-12 13:12:13 -0400

    analises: semantica e sintatica

---
+++ main.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from analise.lexer import AnalisadorLexico
+from sintese.sintatico import AnalisadorSintatico
+from analise.semantica import AnalisadorSemantico
+
+# Realizando etapa de analise lexica
+lexico = AnalisadorLexico()
+lexico.analisa()
+# Realizando etapa de analise sintatica
+sintatico = AnalisadorSintatico()
+sintatico.start()
+# Realizando etapa de analise semantica
+# semantico = AnalisadorSemantico()
+# semantico.analisa()
